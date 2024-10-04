5fed4563618623ad1826e78317d198c07a4b532a
Darijus Daužvardis <dauzva@gmail.com>
2024-10-04 14:22:51 +0000

    added commands parsing

--- sanbox.hs
+++ sanbox.hs
@@ -6,34 +6,56 @@ module Lib2
     emptyState,
     stateTransition
     ) where
+import qualified Data.Char as C
+import qualified Data.List as L
+import Text.Printf (FormatAdjustment(LeftAdjust))
 
 -- | An entity which represets user input.
 -- It should match the grammar from Laboratory work #1.
 -- Currently it has no constructors but you can introduce
 -- as many as needed.
-data Query = Book
+data AddQuery = AddQuery String
+data Query = Query AddQuery | RemoveQuery | ListQuery
 
 -- | The instances are needed basically for tests
 instance Eq Query where
   (==) _ _= False
 
 instance Show Query where
-  show _ = ""
+    show a = 
+		case a of
+			Query (AddQuery e1) -> show e1
+    show _ = ""
 
 -- | Parses user's input.
 -- The function must have tests.
 parseQuery :: String -> Either String Query
-parseQuery _ = Left "Not implemented 2"
+parseQuery str = 
+    case parseAdd str of
+        Left(eAdd) -> Left(eAdd)
+        Right(rAdd) -> Right(rAdd)
+
+
+parseAdd :: String -> Either String Query
+parseAdd str =
+        let
+                cmd = L.takeWhile C.isAlpha str
+                rest = drop (length cmd) str
+        in
+                if cmd == "add" then    Right (Query (AddQuery rest))
+                else                    Left "Invalid command"
+
 
 -- | An entity which represents your program's state.
 -- Currently it has no constructors but you can introduce
 -- as many as needed.
-data State
+data State = State [Query]
+    deriving Show
 
 -- | Creates an initial program's state.
 -- It is called once when the program starts.
 emptyState :: State
-emptyState = error "Not implemented 1"
+emptyState = State []
 
 -- | Updates a state according to a query.
 -- This allows your program to share the state
@@ -41,4 +63,5 @@ emptyState = error "Not implemented 1"
 -- Right contains an optional message to print and
 -- an updated program's state.
 stateTransition :: State -> Query -> Either String (Maybe String, State)
+stateTransition s q = Right(Just (show s), State [q])
 stateTransition _ _ = Left "Not implemented 3"
--- src/Lib2.hs
+++ src/Lib2.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE InstanceSigs #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant bracket" #-}
 module Lib2
     ( Query(..),
     parseQuery,
@@ -6,35 +8,60 @@ module Lib2
     emptyState,
     stateTransition
     ) where
+import qualified Data.Char as C
+import qualified Data.List as L
 
 -- | An entity which represets user input.
 -- It should match the grammar from Laboratory work #1.
 -- Currently it has no constructors but you can introduce
 -- as many as needed.
-data Query
+data Query = 
+    AddQuery String |
+    RemoveQuery String |
+    ListQuery
 
 -- | The instances are needed basically for tests
 instance Eq Query where
   (==) _ _= False
 
 instance Show Query where
-  show _ = ""
+    show (AddQuery a)       = show a
+    show (RemoveQuery a)    = show a
+    show ListQuery          = show "List:"
 
 -- | Parses user's input.
 -- The function must have tests.
 parseQuery :: String -> Either String Query
-parseQuery _ = Left "Not implemented 2"
+parseQuery str = 
+    case parseCmd str of
+        Left(eCmd) -> Left(eCmd)
+        Right(rCmd) -> Right(rCmd) -- <--- continue case stairs
+
+
+parseCmd :: String -> Either String Query
+parseCmd str =
+        let
+                cmd = L.takeWhile C.isAlpha str
+                rest1 = drop (length cmd) str
+                rest2 = L.takeWhile C.isSpace rest1
+                rest = drop (length rest2) rest1
+        in
+                if      cmd == "add"    then Right (AddQuery rest)
+                else if cmd == "remove" then Right (RemoveQuery rest)
+                else if cmd == "list"   then Right ListQuery
+                else                    Left "Invalid command"
 
 
 -- | An entity which represents your program's state.
 -- Currently it has no constructors but you can introduce
 -- as many as needed.
-data State
+data State = State [Query]
+    deriving Show
 
 -- | Creates an initial program's state.
 -- It is called once when the program starts.
 emptyState :: State
-emptyState = error "Not implemented 1"
+emptyState = State []
 
 -- | Updates a state according to a query.
 -- This allows your program to share the state
@@ -42,4 +69,14 @@ emptyState = error "Not implemented 1"
 -- Right contains an optional message to print and
 -- an updated program's state.
 stateTransition :: State -> Query -> Either String (Maybe String, State)
-stateTransition _ _ = Left "Not implemented 3"
+stateTransition (State old) q = 
+    case q of
+        ListQuery ->
+            Right(Just (show (State old)), State old)
+        AddQuery a ->
+            let
+                id = AddQuery ((show (length old + 1))++". "++a)
+                new = State(old++[id])  
+            in
+                Right(Just (show new), new)
+
